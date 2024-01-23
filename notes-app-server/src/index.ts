76542850346fd5da5f59ca13967c0a4ebc9ec61c
ajaykumarim <ajaykumarim2001@gmail.com>
2024-01-23 11:48:55 +0530

    project

---
+++ notes-app-server/src/index.ts
@@ -0,0 +1,65 @@
+import  express  from "express";
+import cors from 'cors';
+import { PrismaClient } from "@prisma/client";
+
+const app=express();
+const prisma=new PrismaClient()
+app.use(express.json());
+app.use(cors())
+
+app.get("/api/notes",async(req,res)=>{
+    const notes=await prisma.note.findMany();
+    res.json(notes)
+})
+app.post("/api/notes",async(req,res)=>{
+    const {title,content}=req.body;
+    if(!title||!content){
+        return res.status(400).send("title and contents fields required")
+    }
+    try{
+        const note=await prisma.note.create({
+            data:{title,content}
+        })
+        res.json(note)
+    }catch(error){
+        res.status(500).send("OOPs something went wrong!")
+    }   
+})
+
+app.put("/api/notes/:id",async(req,res)=>{
+    const {title,content}=req.body;
+    const id =parseInt(req.params.id)
+    if(!title || !content){
+        return res.status(400).send("title and contends field required!")
+    }
+    if(!id||isNaN(id)){
+        return res.status(400).send("ID must be a valid number")
+    }
+    try{
+        const updatedNote=await prisma.note.update({
+            where:{id},
+            data:{title,content}
+        })
+        res.json(updatedNote)
+    }catch(error){
+        res.status(500).send("OOps, something went wrong!")
+    }
+})
+app.delete("/api/notes/:id",async(req,res)=>{
+    const id=parseInt(req.params.id)
+    if(!id||isNaN(id)){
+        return res.status(400).send("Id must be valid integer")
+    }
+    try{
+        await prisma.note.delete({
+            where:{id},
+        })
+        res.status(204).send()
+    }catch(error){
+        res.status(500).send("OOps something wend wrong")
+    }
+})
+const port=5000
+app.listen(port,()=>{
+    console.log("server running on local host:5000")
+})
